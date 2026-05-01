250c2f7b2b0a7135512437bba83a35a0dcf14d8e
Maria Chauhan <mmontoy1@wgu.edu>
2026-05-01 14:59:57 -0500

    updated script.js

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-document.addEventListener("DOMContentLoaded", function() {
-    console.log("Taniti Tourism Website Loaded Successfully");
-});
+function bookNow() {
+    alert("Booking feature coming soon!");
+}
+
+function confirmBooking(event) {
+    event.preventDefault();
+    document.getElementById("confirmation").innerText = "Booking confirmed!";
+}
